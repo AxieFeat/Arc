2084159bf2ab979d2647549ef56845f7a5540df8
AxieFeat <kirill@arial.su>
2025-03-04 14:01:53 +1000

    update demo

--- arc-demo/src/main/kotlin/arc/demo/bind/KeyLogger.kt
+++ arc-demo/src/main/kotlin/arc/demo/bind/KeyLogger.kt
@@ -6,7 +6,7 @@ import arc.input.mouse.MouseBinding
 object KeyLogger : MouseBinding {
 
     override val id: String = "arc.demo.logger"
-    override val key: KeyCode = KeyCode.ANY_KEY
+    override val key: KeyCode = KeyCode.ANY
 
     override fun onPress(key: KeyCode) {
         println("Key: $key")
